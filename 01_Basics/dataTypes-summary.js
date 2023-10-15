3807f9567092d9313b8dbb6bd1b83ee8b0ebef57
C0nfu5ing_5pring <124070657+C0nfu5ing-5pring@users.noreply.github.com>
2023-10-15 04:51:26 +0000

    dataType=summary added

---
+++ 01_Basics/dataTypes-summary.js
@@ -0,0 +1,25 @@
+// Primitive
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// JavaScript is dynamic language
+// This is an example of Symbol
+let id = Symbol("123");
+let anotherId = Symbol("123");
+console.log(id === anotherId); // false
+
+// Reference (Non-Primitive)
+
+//Array, Objects, Functions
+
+// Data types and their returns
+
+// String => string
+// Number => number
+// Boolean => boolean
+// Symbol => symbol
+// null => object
+// undefined => undefined
+// BigInt => bigint
+// Array and Object => object
+// Function => object function
