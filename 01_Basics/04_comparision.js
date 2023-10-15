db1bd40138fd4516617c4df1024f74821a2c112e
C0nfu5ing_5pring <124070657+C0nfu5ing-5pring@users.noreply.github.com>
2023-10-15 04:14:24 +0000

    comparision file added

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1); // true
+// console.log("2" >= 1); // true
+// console.log(2 < 1); // false
+// console.log(2 == 1); // false
+// console.log(2 != 1); // true
+
+
+// console.log(null > 0); // false
+// console.log(null < 0); // false
+// console.log(null >= 0); // true JavaScript converted null to 0 
+
+// console.log(undefined > 90); // false
+// console.log(undefined < 90); // false
+// console.log(undefined == 0); //false 
+//Whenever we compare anything with undefined we always get false as an output.
+
+// The > and < are different from >=, <=, == and === but == is different from ===
+
+// ===
+
+console.log("2" === 2); // false 
+console.log("2" == 2); // false because here conversion takes place.
