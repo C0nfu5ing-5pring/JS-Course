b8a2209241e959bd1d64ae2941c2d1381effcfce
C0nfu5ing_5pring <124070657+C0nfu5ing-5pring@users.noreply.github.com>
2023-10-18 15:37:07 +0000

    added files

---
+++ 01_Basics/06_nums_and_maths.js
@@ -0,0 +1,20 @@
+const score = 123;
+// console.log(score) // 123
+
+const balance = new Number(12343);
+// console.log(balance); // [Number: 12343]
+
+console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 1234.8972;
+// console.log(otherNumber.toPrecision(6)); // Gives first 6 values of the number and if it can round off it will provide the round-offed number.
+
+const balanceTwo = 1000000;
+// console.log(balanceTwo.toLocaleString()); // 1,000,000
+// console.log(balanceTwo.toLocaleString("en-IN")); // 10,00,000
+
+
+/*********************Maths******************/
+
+ 
