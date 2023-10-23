533398b0af2498afdf7520d7fd7c44e17941e0aa
C0nfu5ing_5pring <124070657+C0nfu5ing-5pring@users.noreply.github.com>
2023-10-23 11:02:31 +0000

    new fikes added

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,36 @@
+// Arrays
+// Array is an object.Arrays in JavaScript are resizable meaning, new elements can be added even after it is declared. It can consist of any datatype (including arrays).
+
+// Different ways of declaring arrays.
+const myArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, true, "Shish"]; //Anything put inside the square brackets [] is called element. Element can be of any datatype.
+const myPlayers = ["Lionel Messi", "Antoine Griezmann", "Eduardo Camavinga"];
+const myArr2 = new Array ("Batsurat Pichai", "Bill Gates", "Elon Musk")
+// console.log(myArr[10]);
+
+// Array methods
+
+// myArr.push(9); // Takes one input and adds it to end of an array.
+// myArr.push(10);
+// myArr.push(11);
+// myArr.pop(); // Does not take any input and removes the last element of an array.
+// myArr.unshift(9); // Takes input and adds it to the start of an array.
+// myArr.shift(); // Does not take any input and removes the first element of an array.
+// console.log(myArr);
+
+// questionaire methods that can of JavaScript.
+
+// console.log(myArr.includes(false)); // Takes input and checks whether it is present in the given array or not. Gives output in true and false.  
+
+// console.log(myArr.indexOf("Shish")); // Takes input and gives out the index of the given input in th given array.
+
+// console.log(myArr.indexOf("Aahil")); // Takes input and gives out the index of the given input in th given array. If it does not find the element in the array it will always give output -1.
+
+// const newArr = myArr.join(); // Adds all the elements of an array into a string and binds all the elements.
+
+// console.log(myArr); // [ 0, 1, 2, 3, 4, 5, 6, 7, 8, true, 'Shish' ]
+// console.log(newArr); // 0,1,2,3,4,5,6,7,8,true,Shish 
+// console.log(typeOf(newArr));
+
+//slice(), splice()
+
+
